8e30c32811e9528022b324b3978abe7caf49f1aa
BasiMontes <76871306+BasiMontes@users.noreply.github.com>
2021-05-31 17:17:54 +0200

    Add files via upload

---
+++ mapa_hospitales_Sevilla/data/hosp_sev_3.js
@@ -0,0 +1,19 @@
+var json_hosp_sev_3 = {
+"type": "FeatureCollection",
+"name": "hosp_sev_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "hospital": "Hospital Universitario Virgen del Rocío", "lat": 37.362796757303251, "lon": -5.9777107032603549 }, "geometry": { "type": "Point", "coordinates": [ -5.977710703260355, 37.362796757303251 ] } },
+{ "type": "Feature", "properties": { "id": "2", "hospital": "Hospital Quirónsalud Sagrado Corazón", "lat": 37.361759557797363, "lon": -5.9788549517396996 }, "geometry": { "type": "Point", "coordinates": [ -5.9788549517397, 37.361759557797363 ] } },
+{ "type": "Feature", "properties": { "id": "3", "hospital": "Hospital de Día Quirónsalud Ave María", "lat": 37.35864288619667, "lon": -5.9809733636916977 }, "geometry": { "type": "Point", "coordinates": [ -5.980973363691698, 37.35864288619667 ] } },
+{ "type": "Feature", "properties": { "id": "4", "hospital": "Hospital Victoria Eugenia Cruz Roja Sevilla", "lat": 37.40034620281223, "lon": -5.9828359792445696 }, "geometry": { "type": "Point", "coordinates": [ -5.98283597924457, 37.40034620281223 ] } },
+{ "type": "Feature", "properties": { "id": "5", "hospital": "Hospital Fátima. Clínica Privada", "lat": 37.368982946160095, "lon": -5.9880636378095016 }, "geometry": { "type": "Point", "coordinates": [ -5.988063637809502, 37.368982946160095 ] } },
+{ "type": "Feature", "properties": { "id": "6", "hospital": "Hospital San Juan de Dios", "lat": 37.381641487263714, "lon": -5.9690970605493927 }, "geometry": { "type": "Point", "coordinates": [ -5.969097060549393, 37.381641487263714 ] } },
+{ "type": "Feature", "properties": { "id": "7", "hospital": "Hospital Quirónsalud Infanta Luisa", "lat": 37.381224539422732, "lon": -6.0079279470490592 }, "geometry": { "type": "Point", "coordinates": [ -6.007927947049059, 37.381224539422732 ] } },
+{ "type": "Feature", "properties": { "id": "8", "hospital": "Clínica HLA Santa Isabel", "lat": 37.386785333879772, "lon": -5.9754949397196304 }, "geometry": { "type": "Point", "coordinates": [ -5.97549493971963, 37.386785333879772 ] } },
+{ "type": "Feature", "properties": { "id": "9", "hospital": "Hospital San Lázaro", "lat": 37.414451348408349, "lon": -5.9829110370310534 }, "geometry": { "type": "Point", "coordinates": [ -5.982911037031053, 37.414451348408349 ] } },
+{ "type": "Feature", "properties": { "id": "10", "hospital": "Hospital Virgen De Valme", "lat": 37.3188297479958, "lon": -5.9712114954733622 }, "geometry": { "type": "Point", "coordinates": [ -5.971211495473362, 37.3188297479958 ] } },
+{ "type": "Feature", "properties": { "id": "11", "hospital": "Hospital Fátima Sevilla Este", "lat": 37.399988099857531, "lon": -5.9273503373815624 }, "geometry": { "type": "Point", "coordinates": [ -5.927350337381562, 37.399988099857531 ] } },
+{ "type": "Feature", "properties": { "id": "12", "hospital": "Hospital Duques del Infantado", "lat": 37.36308554314526, "lon": -5.9876417409432001 }, "geometry": { "type": "Point", "coordinates": [ -5.9876417409432, 37.36308554314526 ] } }
+]
+}
